24307bab62cf3ce167105fe4929377a5ca716ad9
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-05-12 12:28:13 +0200

    creates observable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
-import { HomeComponent } from './home/home.component';
+import {AppComponent} from './app.component';
+import {UserComponent} from './user/user.component';
+import {HomeComponent} from './home/home.component';
 
 
 @NgModule({
@@ -19,4 +19,5 @@ import { HomeComponent } from './home/home.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
